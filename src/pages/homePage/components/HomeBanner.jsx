119287581e8d04fa588d4f981bb5091fa94e8dfc
Muhammad Awais <140505575+awaisdev6330@users.noreply.github.com>
2024-04-24 19:56:01 +0500

    assets folder move in public

--- src/pages/homePage/components/HomeBanner.jsx
+++ src/pages/homePage/components/HomeBanner.jsx
@@ -5,7 +5,7 @@ export default function HomeBannerSection() {
         <img
           src="https://images.unsplash.com/photo-1521706862577-47b053587f91?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
-          className="aspect-[5/2] w-full object-cover xl:rounded-3xl"
+          className="aspect-[5/1] w-full object-cover xl:rounded-3xl"
         />
       </div>
     </>
--- src/pages/homePage/components/HomeGallary.jsx
+++ src/pages/homePage/components/HomeGallary.jsx
@@ -78,7 +78,7 @@ export default function HomeGallerySection() {
             </div>
             <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg bg-gray-100">
               <img
-                src="https://images.unsplash.com/photo-1547575824-440930b53b4d?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src="../../../../public/assets/images/about-us.jpg"
                 alt=""
                 className="object-cover object-center"
               />
